478c7e3fbac61c509f33cf4b10c7043132096f9b
tomasjur <tomasjkaunas@gmail.com>
2018-03-16 00:07:31 +0200

    Added categories table to 'islaidos' database

---
+++ database/migrations/islaidos/2014_10_12_200000_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomas
+ * Date: 2018-03-15
+ * Time: 15:21
+ */
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
