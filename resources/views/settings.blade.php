80d6c51c7d62689903c2554329b202b18b578ee5
tomasjur <tomasjkaunas@gmail.com>
2018-03-22 01:26:13 +0200

    Updated settings page. Fixed logo url

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,9 +1,21 @@
-<html>
+@extends('layouts.app')
+
 <!--
  * Created by PhpStorm.
  * User: Tomas
  * Date: 2018-03-22
  * Time: 00:03
  -->
-Settings
-</html>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Settings</div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
