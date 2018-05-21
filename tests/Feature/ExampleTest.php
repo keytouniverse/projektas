0e94606e33788eb453aac8484dc68a1a5fc27e19
DESKTOP-S7DN64Q\Dinas <Dinas.Janisovas@gmail.com>
2018-05-21 20:24:14 +0300

    Added Expenses page testing. PHPUnit Test consists of logging in as a fake user, adding two random amounts to spent money and checking whether sum is displayed anywhere on the page.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Tests\Feature;
+namespace App;
 
 use Tests\TestCase;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -17,6 +18,7 @@ class ExampleTest extends TestCase
         $this->visit('/')
             ->click('Register')
             ->seePageIs('/register');
+        echo "Redirection is working...";
     }
 
     public function testRegistration()
@@ -31,6 +33,30 @@ class ExampleTest extends TestCase
                 ->type('secret', 'password')
                 ->type('secret', 'password_confirmation')
                 ->press('Register')
+            // Checks whether registration is successful.
                 ->seePageIs('/home');
+        echo "\nRegistration is working as intended...";
+    }
+
+    public function testExpenses()
+    {
+        $user = factory(User::class)->create();
+        $spentMoney1 = rand(1,600);
+        $spentMoney2 = rand(1,600);
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/expenses')
+            // Checks whether the username is visible somewhere on the page
+            ->see($user->name)
+            ->type((string)$spentMoney1,'amount')
+            ->select(rand(1,10),'category')
+            ->press('ADD')
+            ->type((string)$spentMoney2,'amount')
+            ->select(rand(1,10),'category')
+            ->press('ADD')
+            // Checks whether the added values are visible somewhere on the Expenses page
+            ->see($spentMoney1+$spentMoney2);
+        echo "\n'Expenses' page functions correctly...";
     }
 }
